7d72fc057238701736695c86d1b173b869215548
Welberty Ferraz Vieira Lopes <welberty.betinho@gmail.com>
2020-12-16 09:54:08 -0300

    (core) adding bugs

--- core/index.js
+++ core/index.js
@@ -1,5 +1,33 @@
 const core = () => {
     return "This is a core module"
 }
+const core = () => {
+    return "This is a core module"
+}
+
+const core = () => {
+    return "This is a core module"
+}
+
+const core = () => {
+    return "This is a core module"
+}
+
+const core = () => {
+    return "This is a core module"
+}
+
+const core = () => {
+    return "This is a core module"
+}
+
+const core = () => {
+    return "This is a core module"
+}
+
+const core = () => {
+    return "This is a core module"
+}
+
 
 module.exports = {core}
